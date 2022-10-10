ff258f85d38069c9638035b5d405989ac00d4bbf
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-10-10 12:43:42 -0400

    initial commit

---
+++ python_OOP/forLoop.py
@@ -0,0 +1,15 @@
+# for count in range(0,151):
+#     if count%5==0:
+#         print(count)
+
+# for count in range(0,101):
+#     if count%10==0 and count%5==0:
+#         print('Dojo')
+#     elif count%5==0:
+#         print('Coding')
+#     else:
+#         print(count)
+sum=0
+for count in range(0,1000):
+    sum += count
+    print(sum)
