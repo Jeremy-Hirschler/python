fa31de9e5385dc0ffefe5dc4d02e22865caa97cb
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-10-11 19:59:53 -0400

    initial commit

---
+++ flask_fundamentals/fruits/server.py
@@ -0,0 +1,22 @@
+from distutils.log import debug
+from django.shortcuts import render
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    print(request.form)
+    straw = int(request.form['strawberry'])
+    rasp = int(request.form['raspberry'])
+    apple = int(request.form['apple'])
+    total = straw + rasp + apple
+    return render_template('checkout.html', total=total, straw=straw, rasp=rasp, apple=apple)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
