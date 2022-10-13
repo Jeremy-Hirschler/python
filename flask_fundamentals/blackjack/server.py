4f0a557ce719f65ade45e4a951517bf830d0a5cb
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-10-13 19:40:27 -0400

    removed js functionality in favor of flask session

--- flask_fundamentals/blackjack/server.py
+++ flask_fundamentals/blackjack/server.py
@@ -1,8 +1,9 @@
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, session
 import random
 
 app = Flask(__name__)
+app.secret_key = 'YourFateWasSealedWhenYouRoseAgainstUs!'
 
 @app.route('/')
 def welcome():
@@ -11,13 +12,29 @@ def welcome():
 @app.route('/start')
 def new_game():
     card_stack = [
-        'A (11 or 1)','K (10)','Q (10)','J (10)','10',
-        '9','8','7','6','5','4','3','2'
+        # 'A (11 or 1)','K (10)','Q (10)','J (10)',
+        '10','9','8','7','6','5','4','3','2'
     ]
-    card = random.choice(card_stack)
-    print(card)
-    sum = card
-    return render_template('game.html',card=card, sum=sum)
+    session['stack'] = card_stack
+    if 'card' not in session:
+        session['card'] = random.choice(card_stack)
+    if 'sum' not in session:
+        session['sum'] = int(session['card'])
+    return render_template('game.html')
+
+@app.route('/add_card', methods=['POST'])
+def new_card():
+    session['new_card'] = request.form['play']
+    if session['new_card'] == 'hit':
+        session['card'] = random.choice(session['stack'])
+        
+        session['sum'] = session['sum'] + int(session['card'])
+        
+
+    else:
+        session.clear()
+    return redirect('/start')
+
 
 if __name__=='__main__':
     app.run(debug=True)
