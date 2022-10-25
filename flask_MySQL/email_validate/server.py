bb7d3544c9ae370e4ba332b6518e2cd8a06b10b4
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-10-25 19:33:43 -0400

    added delete route and flash for email validation

--- flask_MySQL/email_validate/server.py
+++ flask_MySQL/email_validate/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, session, redirect, render_template, request
+from flask import Flask, session, redirect, render_template, request, flash
 from mysqlconnection import connectToMySQL
-
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 app = Flask(__name__)
 app.secret_key = 'OSW'
@@ -14,12 +15,28 @@ def index():
 @app.route('/add_email', methods=['POST'])
 def add_email():
     mysql = connectToMySQL('emails')
+    if not EMAIL_REGEX.match(request.form['email']):
+        flash('Invalid email address!')
+
     data = {'email': request.form['email']}
     
     query = 'INSERT INTO user_emails (email_address) VALUES (%(email)s)'
+    print(query)
 
-    new_id = mysql.query_db(query, data)
-    print(new_id)
+    if '_flashes' not in session.keys():
+        new_id = mysql.query_db(query, data)
+        print(new_id)
+        return redirect('/')
+    else:
+        return redirect('/')
+        
+
+@app.route('/delete/<int:id>', methods=['GET'])
+def delete_user(id):
+    mysql = connectToMySQL('emails')
+    print('id is:',id)
+    query = 'DELETE FROM user_emails WHERE id = {}'.format(id)
+    delete_user = mysql.query_db(query)
     return redirect('/')
 
 if __name__ == '__main__':
